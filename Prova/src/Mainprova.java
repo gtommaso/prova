306857cb858798d79d81441a454d4beaa92e3272
MacBook <MacBook@mbp-di-tommaso>
2017-09-11 22:28:24 +0200

    commit sviluppo

--- Prova/src/Mainprova.java
+++ Prova/src/Mainprova.java
@@ -3,7 +3,7 @@ public class Mainprova {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.println("hello");
+   System.out.println("HELLO WORD");
 	}
 
 }
